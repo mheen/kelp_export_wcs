288c188828526eb7368e723e448c79eb3db9b2e9
mirjam <mirjam.vandermheen@uwa.edu.au>
2022-12-31 18:10:29 +0800

    Moved plot functions from Particles class to separate script

--- particles.py
+++ particles.py
@@ -1,19 +1,7 @@
-from basic_maps import perth_map
-from bathymetry_data import BathymetryData
+from tools.timeseries import convert_time_to_datetime
+from tools import log
 from netCDF4 import Dataset
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import matplotlib.dates as mdates
-import cartopy.crs as ccrs
-import numpy as np
-from warnings import warn
-
-import sys
-sys.path.append('..')
-from py_tools.timeseries import convert_time_to_datetime
-from py_tools.files import get_dir_from_json
-from py_tools import log
 
 class Particles:
     def __init__(self, time:np.ndarray,
@@ -21,27 +9,19 @@ class Particles:
                  lon:np.ndarray,
                  lat:np.ndarray,
                  z:np.ndarray,
-                 h:np.ndarray,
-                 u: np.ndarray,
-                 v: np.ndarray,
                  salt:np.ndarray,
                  temp:np.ndarray,
-                 age:np.ndarray,
-                 origin_marker:np.ndarray):
+                 age:np.ndarray):
         self.time = time
         self.status = status # 0: active, 1: stranded, -999: not active yet or out of domain
-        self.lon = lon
-        self.lat = lat
-        self.z = z
+        self.lon = lon # [trajectory, time]
+        self.lat = lat # [trajectory, time]
+        self.z = z # [trajectory, time]
 
-        self.h = h
-        self.u = u
-        self.v = v
         self.salt = salt
         self.temp = temp
         
         self.age = age # [days]
-        self.origin_marker = origin_marker
 
         self.add_initial_positions()
 
@@ -67,16 +47,12 @@ class Particles:
         lat = self.lat[l_depth, :]
         z = self.z[l_depth, :]
 
-        h = self.h[l_depth, :]
-        u = self.u[l_depth, :]
-        v = self.v[l_depth, :]
         salt = self.salt[l_depth, :]
         temp = self.temp[l_depth, :]
 
         age = self.age[l_depth, :]
-        origin_marker = self.origin_marker[l_depth, :]
 
-        return Particles(self.time, status, lon, lat, z, h, u, v, salt, temp, age, origin_marker)
+        return Particles(self.time, status, lon, lat, z, salt, temp, age)
 
     def filter_based_on_release_lon_lat_range(self, lon_range:list, lat_range:list):
         l_lon_range = np.logical_and(self.lon0>=lon_range[0], self.lon0<=lon_range[1])
@@ -88,16 +64,12 @@ class Particles:
         lat = self.lat[l_range, :]
         z = self.z[l_range, :]
 
-        h = self.h[l_range, :]
-        u = self.u[l_range, :]
-        v = self.v[l_range, :]
         salt = self.salt[l_range, :]
         temp = self.temp[l_range, :]
 
         age = self.age[l_range, :]
-        origin_marker = self.origin_marker[l_range, :]
 
-        return Particles(self.time, status, lon, lat, z, h, u, v, salt, temp, age, origin_marker)
+        return Particles(self.time, status, lon, lat, z, salt, temp, age)
 
     def get_l_deep_sea(self, h_deep_sea:float) -> np.ndarray:
         i_ds, j_ds = np.where(self.z <= -h_deep_sea)
@@ -161,187 +133,6 @@ class Particles:
 
         return n_particles_in_sim
 
-    def plot_matrix_arriving_in_deep_sea(self, h_deep_sea:float,
-                                         ax=None, show=True, cmap='plasma',
-                                         output_path=None) -> plt.axes:
-        if ax is None:
-            fig = plt.figure(figsize=(10, 5))
-            ax = plt.axes()
-        
-        time_release, age_arriving_ds, matrix_arriving_ds = self.get_matrix_release_age_arriving_deep_sea(h_deep_sea)
-        n_dim_matrix = len(matrix_arriving_ds.flatten().shape)
-        if n_dim_matrix == 1:
-            warn(f'''There is only a single release available in this simulation;
-                 matrix_arriving_ds is {n_dim_matrix}D, so this plot cannot be made. Skipping.''')
-            return ax
-
-        n_particles_in_simulation = self.get_n_particles_in_simulation()
-
-        y, x = np.meshgrid(age_arriving_ds, time_release)
-        z = matrix_arriving_ds/n_particles_in_simulation[0]
-        z[z==0] = np.nan
-
-        c = ax.pcolormesh(x, y, z, cmap=cmap)
-        cbar = plt.colorbar(c)
-        cbar.set_label('Fraction of released particles arriving in deep sea')
-
-        def _create_ticklabels_at_interval(tick_labels:np.ndarray, n_int:int) -> list:
-            tick_labels_int = []
-            for i in range(len(tick_labels)):
-                if i%n_int == 0: # (% is modulo)
-                    tick_labels_int.append(str(tick_labels[i]))
-                else:
-                    tick_labels_int.append('')
-            return tick_labels_int
-
-        myFmt = mdates.DateFormatter('%Y-%m-%d')
-        ax.set_xticks(time_release[::5])
-        ax.set_xticklabels(time_release[::5], rotation=90)
-        ax.xaxis.set_major_formatter(myFmt)
-        ax.set_xlabel('Release date')
-        ax.set_xlim([time_release[0], time_release[-1]])
-
-        ax.set_yticks(age_arriving_ds)
-        ax.set_yticklabels(_create_ticklabels_at_interval(age_arriving_ds.astype(int), 5))
-        ax.set_ylabel('Particle age (days)')
-        ax.set_ylim([0, np.nanmax(age_arriving_ds)])
-
-        if output_path is not None:
-            log.info(f'Saving figure to: {output_path}')
-            plt.savefig(output_path, bbox_inches='tight', dpi=300)
-
-        if show is True:
-            plt.show()
-        else:
-            return ax
-
-    def plot_age_in_deep_sea(self, h_deep_sea:float, total_particles=None,
-                             linestyle='-', color='k', label='',
-                             ax=None, show=True, output_path=None) -> plt.axes:
-        if ax is None:
-            fig = plt.figure(figsize=(10, 5))
-            ax = plt.axes()
-            ax.set_xlabel('Particle age (days)')
-            ax.set_ylabel('Fraction in deep sea')
-
-        _, age_arriving_ds, matrix_arriving_ds = self.get_matrix_release_age_arriving_deep_sea(h_deep_sea)
-        n_dim_matrix = len(matrix_arriving_ds.shape)
-        if n_dim_matrix == 2:
-            n_deep_sea_per_age = np.cumsum(np.sum(matrix_arriving_ds, axis=0))
-        elif n_dim_matrix == 1:
-            n_deep_sea_per_age = np.cumsum(matrix_arriving_ds)
-        else:
-            raise ValueError(f'You should not be able to get here: matrix_arriving_ds is {n_dim_matrix}D')
-
-        if total_particles is None:
-            total_particles = self.lon.shape[0]
-        f_deep_sea_per_age = n_deep_sea_per_age/total_particles # divided by total # particles
-
-        ax.plot(age_arriving_ds, f_deep_sea_per_age, linestyle, color=color, label=label)
-
-        if output_path is not None:
-            log.info(f'Saving figure to: {output_path}')
-            plt.savefig(output_path, bbox_inches='tight', dpi=300)
-
-        if show is True:
-            plt.show()
-        else:
-            return ax
-
-    def plot_timeseries_in_deep_sea(self, h_deep_sea:float, total_particles=None,
-                                    ax=None, show=True, output_path=None) -> plt.axes:
-        if ax is None:
-            fig = plt.figure(figsize=(10, 5))
-            fig.tight_layout()
-            ax = plt.axes()
-            ax.set_ylabel('# of particles')
-
-        n_deep_sea = self.get_n_deep_sea(h_deep_sea)
-        n_particles_sim = self.get_n_particles_in_simulation()
-
-        ax.plot(self.time, n_deep_sea, '-k', label='# in deep sea')
-        ax.plot(self.time, n_particles_sim, '--k', label='# in simulation')
-        ax.legend(loc='upper left')
-
-        color_ax2='g'
-        ax2 = ax.twinx()
-        ax2.set_ylabel('Fraction in deep sea', color=color_ax2)
-        if total_particles is None:
-            total_particles = self.lon.shape[0]
-        ax2.plot(self.time, n_deep_sea/total_particles, '-', color=color_ax2)
-        ax2.tick_params(axis='y', labelcolor=color_ax2)
-
-        if output_path is not None:
-            log.info(f'Saving figure to: {output_path}')
-            plt.savefig(output_path, bbox_inches='tight', dpi=300)
-
-        if show is True:
-            plt.show()
-        else:
-            return ax
-
-    def plot_initial(self, ax=None, show=True, color='k') -> plt.axes:
-        if ax is None:
-            ax = plt.axes(projection=ccrs.PlateCarree())
-            ax = perth_map(ax)
-
-        ax.plot(self.lon0, self.lat0, '.', color=color)
-
-        if show is True:
-            plt.show()
-        else:
-            return ax
-
-    def animate(self, color_p='k', color_i='g', show_bathymetry=True,
-            show_initial=True, output_path=None,
-            dpi=100, fps=25):
-    
-        writer = animation.PillowWriter(fps=fps)
-
-        # plot map
-        plt.rcParams.update({'font.size' : 15})
-        plt.rcParams.update({'font.family': 'arial'})
-        plt.rcParams.update({'figure.dpi': dpi})
-        fig = plt.figure(figsize=(10,8))
-        ax = plt.axes(projection=ccrs.PlateCarree())
-        ax = perth_map(ax)
-
-        if show_bathymetry is True:
-            bathymetry = BathymetryData.read_from_netcdf()
-            ax = bathymetry.plot_contours(ax, show=False, color='#757575')
-        
-        if show_initial is True:
-            ax.plot(self.lon0, self.lat0, 'o', color=color_i, markersize=2, zorder=1, label='initial')
-            ax.legend(loc='upper right')
-
-        # animated points
-        point = ax.plot([], [], 'o', color=color_p, markersize=2, zorder=2)[0]
-
-        # animated text
-        ttl = ax.text(0.5, 1.04,'', transform=ax.transAxes,
-                    ha='center', va='top',
-                    bbox=dict(facecolor='w', alpha=0.3, edgecolor='w', pad=2))
-        ttl.set_animated(True)
-        
-        def init():
-            point.set_data([],[])
-            ttl.set_text('')
-            return point, ttl
-
-        def animate(i):        
-            x, y = (self.lon[:,i], self.lat[:,i])
-            point.set_data(x,y)
-            title = self.time[i].strftime('%d-%m-%Y %H:%M')
-            ttl.set_text(title)
-            return point, ttl
-
-        anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init, frames=len(self.time), blit=True)
-        if output_path is not None:
-            log.info(f'Saving animation to: {output_path}')
-            anim.save(output_path, writer=writer)
-        else:
-            plt.show()
-
     @staticmethod
     def read_from_netcdf(input_path:str):
         log.info(f'Reading Particles from {input_path}')
@@ -357,32 +148,11 @@ class Particles:
         lat = netcdf['lat'][:].filled(fill_value=np.nan)
         z = netcdf['z'][:].filled(fill_value=np.nan)
 
-        h = netcdf['sea_floor_depth_below_sea_level'][:].filled(fill_value=np.nan)
-        u =  netcdf['x_sea_water_velocity'][:].filled(fill_value=np.nan)
-        v = netcdf['y_sea_water_velocity'][:].filled(fill_value=np.nan)
         salt = netcdf['sea_water_salinity'][:].filled(fill_value=np.nan)
         temp = netcdf['sea_water_temperature'][:].filled(fill_value=np.nan)
 
         age = netcdf['age_seconds'][:].filled(fill_value=np.nan)/(24*60*60) # convert age in seconds to age in days
-        origin_marker = netcdf['origin_marker'][:].filled(fill_value=-999)
 
         netcdf.close()
 
-        return Particles(time, status, lon, lat, z, h, u, v, salt, temp, age, origin_marker)
-
-if __name__ == '__main__':
-    h_deep_sea = 150 # m depth
-    file_name = 'perth_2017'
-    get_animation = False
-
-    p = Particles.read_from_netcdf(f'{get_dir_from_json("input/dirs.json", "opendrift")}{file_name}.nc')
-
-    if get_animation is True:
-        animation_path = f'{get_dir_from_json("input/dirs.json", "plots")}{file_name}.gif'
-        p.animate(output_path=animation_path)
-
-    p.plot_timeseries_in_deep_sea(h_deep_sea, show=False, output_path=f'{get_dir_from_json("input/dirs.json", "plots")}{file_name}_timeseries_deep_sea.jpg')
-
-    p.plot_matrix_arriving_in_deep_sea(h_deep_sea, show=False, output_path=f'{get_dir_from_json("input/dirs.json", "plots")}{file_name}_matrix_arriving_deep_sea.jpg')
-
-    p.plot_age_in_deep_sea(h_deep_sea, show=False, output_path=f'{get_dir_from_json("input/dirs.json", "plots")}{file_name}_age_deep_sea.jpg')
+        return Particles(time, status, lon, lat, z, salt, temp, age)
---
+++ plots_particles.py
@@ -0,0 +1,224 @@
+from tools.timeseries import get_closest_time_index
+from tools.files import get_dir_from_json
+from tools import log
+from particles import Particles
+from bathymetry_data import BathymetryData
+from kelp_map import KelpProbability
+from location_info import LocationInfo, get_location_info
+from basic_maps import plot_basic_map
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.dates as mdates
+import cartopy.crs as ccrs
+from warnings import warn
+import numpy as np
+from datetime import datetime
+
+def animate_particles(particles:Particles, location_info:LocationInfo,
+                      show_bathymetry=True, show_kelp_map=True,
+                      output_path=None, color_p='k',
+                      dpi=100, fps=25):
+
+    writer = animation.PillowWriter(fps=fps)
+
+    # plot map
+    plt.rcParams.update({'font.size' : 15})
+    plt.rcParams.update({'font.family': 'arial'})
+    plt.rcParams.update({'figure.dpi': dpi})
+    fig = plt.figure(figsize=(10,8))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax = plot_basic_map(ax, location_info)
+
+    if show_bathymetry is True:
+        bathymetry = BathymetryData.read_from_netcdf('input/cwa_roms_grid.nc')
+        ax = bathymetry.plot_contours(location_info, ax=ax, show=False, color='#757575')
+    
+    if show_kelp_map is True:
+        kelp_prob = KelpProbability.read_from_tiff('input/perth_kelp_probability.tif')
+        ax = kelp_prob.plot(location_info, ax=ax, show=False)
+
+    # animated points
+    point = ax.plot([], [], 'o', color=color_p, markersize=2, zorder=2)[0]
+
+    # animated text
+    ttl = ax.text(0.5, 1.04,'', transform=ax.transAxes,
+                ha='center', va='top',
+                bbox=dict(facecolor='w', alpha=0.3, edgecolor='w', pad=2))
+    ttl.set_animated(True)
+    
+    def init():
+        point.set_data([],[])
+        ttl.set_text('')
+        return point, ttl
+
+    def animate(i):        
+        x, y = (particles.lon[:,i], particles.lat[:,i])
+        point.set_data(x,y)
+        title = particles.time[i].strftime('%d-%m-%Y %H:%M')
+        ttl.set_text(title)
+        return point, ttl
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init, frames=len(particles.time), blit=True)
+    if output_path is not None:
+        log.info(f'Saving animation to: {output_path}')
+        anim.save(output_path, writer=writer)
+    else:
+        plt.show()
+
+def plot_matrix_arriving_in_deep_sea(particles:Particles, h_deep_sea:float,
+                                     ax=None, show=True, cmap='plasma',
+                                     output_path=None) -> plt.axes:
+    if ax is None:
+        fig = plt.figure(figsize=(10, 5))
+        ax = plt.axes()
+    
+    time_release, age_arriving_ds, matrix_arriving_ds = particles.get_matrix_release_age_arriving_deep_sea(h_deep_sea)
+    n_dim_matrix = len(matrix_arriving_ds.flatten().shape)
+    if n_dim_matrix == 1:
+        warn(f'''There is only a single release available in this simulation;
+                matrix_arriving_ds is {n_dim_matrix}D, so this plot cannot be made. Skipping.''')
+        return ax
+
+    n_particles_in_simulation = particles.get_n_particles_in_simulation()
+
+    y, x = np.meshgrid(age_arriving_ds, time_release)
+    z = matrix_arriving_ds/n_particles_in_simulation[0]
+    z[z==0] = np.nan
+
+    c = ax.pcolormesh(x, y, z, cmap=cmap)
+    cbar = plt.colorbar(c)
+    cbar.set_label('Fraction of released particles arriving in deep sea')
+
+    def _create_ticklabels_at_interval(tick_labels:np.ndarray, n_int:int) -> list:
+        tick_labels_int = []
+        for i in range(len(tick_labels)):
+            if i%n_int == 0: # (% is modulo)
+                tick_labels_int.append(str(tick_labels[i]))
+            else:
+                tick_labels_int.append('')
+        return tick_labels_int
+
+    myFmt = mdates.DateFormatter('%Y-%m-%d')
+    ax.set_xticks(time_release[::5])
+    ax.set_xticklabels(time_release[::5], rotation=90)
+    ax.xaxis.set_major_formatter(myFmt)
+    ax.set_xlabel('Release date')
+    ax.set_xlim([time_release[0], time_release[-1]])
+
+    ax.set_yticks(age_arriving_ds)
+    ax.set_yticklabels(_create_ticklabels_at_interval(age_arriving_ds.astype(int), 5))
+    ax.set_ylabel('Particle age (days)')
+    ax.set_ylim([0, np.nanmax(age_arriving_ds)])
+
+    if output_path is not None:
+        log.info(f'Saving figure to: {output_path}')
+        plt.savefig(output_path, bbox_inches='tight', dpi=300)
+
+    if show is True:
+        plt.show()
+    else:
+        return ax
+
+def plot_age_in_deep_sea(particles:Particles, h_deep_sea:float, total_particles=None,
+                         linestyle='-', color='k', label='',
+                         ax=None, show=True, output_path=None) -> plt.axes:
+    if ax is None:
+        fig = plt.figure(figsize=(10, 5))
+        ax = plt.axes()
+        ax.set_xlabel('Particle age (days)')
+        ax.set_ylabel('Fraction in deep sea')
+
+    _, age_arriving_ds, matrix_arriving_ds = particles.get_matrix_release_age_arriving_deep_sea(h_deep_sea)
+    n_dim_matrix = len(matrix_arriving_ds.shape)
+    if n_dim_matrix == 2:
+        n_deep_sea_per_age = np.cumsum(np.sum(matrix_arriving_ds, axis=0))
+    elif n_dim_matrix == 1:
+        n_deep_sea_per_age = np.cumsum(matrix_arriving_ds)
+    else:
+        raise ValueError(f'You should not be able to get here: matrix_arriving_ds is {n_dim_matrix}D')
+
+    if total_particles is None:
+        total_particles = particles.lon.shape[0]
+    f_deep_sea_per_age = n_deep_sea_per_age/total_particles # divided by total # particles
+
+    ax.plot(age_arriving_ds, f_deep_sea_per_age, linestyle, color=color, label=label)
+
+    if output_path is not None:
+        log.info(f'Saving figure to: {output_path}')
+        plt.savefig(output_path, bbox_inches='tight', dpi=300)
+
+    if show is True:
+        plt.show()
+    else:
+        return ax
+
+def plot_timeseries_in_deep_sea(particles:Particles, h_deep_sea:float, total_particles=None,
+                                ax=None, show=True, output_path=None) -> plt.axes:
+    if ax is None:
+        fig = plt.figure(figsize=(10, 5))
+        fig.tight_layout()
+        ax = plt.axes()
+        ax.set_ylabel('# of particles')
+
+    n_deep_sea = particles.get_n_deep_sea(h_deep_sea)
+    n_particles_sim = particles.get_n_particles_in_simulation()
+
+    ax.plot(particles.time, n_deep_sea, '-k', label='# in deep sea')
+    ax.plot(particles.time, n_particles_sim, '--k', label='# in simulation')
+    ax.legend(loc='upper left')
+
+    color_ax2='g'
+    ax2 = ax.twinx()
+    ax2.set_ylabel('Fraction in deep sea', color=color_ax2)
+    if total_particles is None:
+        total_particles = particles.lon.shape[0]
+    ax2.plot(particles.time, n_deep_sea/total_particles, '-', color=color_ax2)
+    ax2.tick_params(axis='y', labelcolor=color_ax2)
+
+    if output_path is not None:
+        log.info(f'Saving figure to: {output_path}')
+        plt.savefig(output_path, bbox_inches='tight', dpi=300)
+
+    if show is True:
+        plt.show()
+    else:
+        return ax
+
+def plot_particle_locations(particles:Particles, location_info:LocationInfo,
+                            time=None, t=None,
+                            ax=None, show=True, color='k') -> plt.axes:
+    if ax is None:
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax = plot_basic_map(ax, location_info)
+
+    if t is None and time is None:
+        log.info('''Plotting initial particles locations. Specify either requested
+                 time or time index t if you want to plot a different time.''')
+        ax.plot(particles.lon0, particles.lat0, '.', color=color)
+    elif t == 0:
+        ax.plot(particles.lon0, particles.lat0, '.', color=color)
+    elif t != 0:
+        ax.plot(particles.lon[:, t], particles.lat[:, t], '.', color=color)
+    elif t is None and time is not None:
+        t = get_closest_time_index(particles.time, time)
+        ax.plot(particles.lon[:, t], particles.lat[:, t], '.', color=color)
+
+    if show is True:
+        plt.show()
+    else:
+        return ax
+
+if __name__ == '__main__':
+    location_info = get_location_info('cwa_perth')
+    h_deep_sea = 200 # m depth: edge of continental shelf
+
+    input_path = f'{get_dir_from_json("opendrift")}cwa-perth_2017-Mar-Aug.nc'
+    particles = Particles.read_from_netcdf(input_path)
+    
+    animation_path = f'{get_dir_from_json("plots")}animation_cwa-perth_2017-Mar-Aug.gif'
+    animate_particles(particles, location_info, output_path=animation_path)
+
+    # plot_particle_locations(particles, location_info, t=0)
+
+    # plot_age_in_deep_sea(particles, h_deep_sea)
+    
