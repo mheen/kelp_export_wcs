574a1b6b0f02093023307b51e6af2ca902b9aeb9
mirjam <mirjam.vandermheen@uwa.edu.au>
2023-01-03 09:28:21 +0800

    Changed bathymetry colormap

--- plots_bathymetry.py
+++ plots_bathymetry.py
@@ -114,4 +114,4 @@ if __name__ == '__main__':
     output_path = f'{get_dir_from_json("plots")}cwa-perth-zoom_bathymetry.jpg'
     _ = plot_bathymetry_and_contours(bathymetry.lon, bathymetry.lat, bathymetry.h, location_info,
                                      show=False, output_path=output_path, vmin=0, vmax=4000,
-                                     contour_fontsize=17)
+                                     contour_fontsize=17, cmap='viridis_r')
