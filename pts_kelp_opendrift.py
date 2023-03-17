2ef429026048016a4c9f8a00881cb98740419b20
mirjam <mirjam.vandermheen@uwa.edu.au>
2023-03-17 15:38:37 +0800

    Moved opendrift tools and release functions to pts_tools/ folder

--- pts_kelp_opendrift.py
+++ pts_kelp_opendrift.py
@@ -1,9 +1,9 @@
 from tools.files import get_dir_from_json
 from tools.timeseries import add_month_to_time
 from tools import log
-from readers import reader_ROMS
-from models.opendrift_bottomdrifters import BottomDrifters
-from data.bathymetry_data import BathymetryData
+from pts_tools import opendrift_reader_ROMS as reader_ROMS
+from pts_tools.opendrift_bottomdrifters import BottomDrifters
+from pts_tools.releases import get_n_daily_release_times
 from data.kelp_data import generate_random_releases_based_on_probability
 from datetime import datetime, timedelta
 import numpy as np
@@ -19,37 +19,6 @@ import sys
 # lon_release = 115.68
 # lat_release = -31.78
 # release_description = 'marmion_litterbag'
-
-def get_lon_lat_release_roms_depth(min_depth:float, max_depth:float) -> tuple:
-    bathymetry = BathymetryData.read_from_csv()
-
-    l_depth = np.logical_and(min_depth<bathymetry.h, bathymetry.h<max_depth)
-    lon0 = bathymetry.lon[l_depth]
-    lat0 = bathymetry.lat[l_depth]
-    return lon0, lat0
-
-def get_n_hourly_release_times(year:int, month:int, day:int, n_months=1, n_hours=3) -> np.ndarray:
-    start_date = datetime(year, month, day)
-    end_date = add_month_to_time(start_date, n_months)
-    n_days = (end_date-start_date).days
-    n_hours = int(n_days*24/n_hours-n_hours)
-
-    release_times = []
-    for i in range(n_hours):
-        release_times.append(start_date+timedelta(hours=i))
-
-    return np.array(release_times)
-
-def get_n_daily_release_times(year:int, month:int, day:int, n_months=4, n_days=1) -> np.ndarray:
-    start_date = datetime(year, month, day)
-    end_date = add_month_to_time(start_date, n_months)
-    n_days = (end_date-start_date).days
-
-    release_times = []
-    for i in range(n_days):
-        release_times.append(start_date+timedelta(days=i))
-
-    return np.array(release_times)
 
 def run(release_times:np.ndarray,
         lon_releases:np.ndarray,
---
+++ pts_tools/releases.py
@@ -0,0 +1,39 @@
+import os, sys
+parent = os.path.abspath('.')
+sys.path.insert(1, parent)
+
+from tools.timeseries import add_month_to_time
+from data.bathymetry_data import BathymetryData
+from datetime import datetime, timedelta
+import numpy as np
+
+def get_lon_lat_releases_based_on_depth(min_depth:float, max_depth:float) -> tuple:
+    bathymetry = BathymetryData.read_from_csv()
+
+    l_depth = np.logical_and(min_depth<bathymetry.h, bathymetry.h<max_depth)
+    lon0 = bathymetry.lon[l_depth]
+    lat0 = bathymetry.lat[l_depth]
+    return lon0, lat0
+
+def get_n_hourly_release_times(year:int, month:int, day:int, n_months=1, n_hours=3) -> np.ndarray:
+    start_date = datetime(year, month, day)
+    end_date = add_month_to_time(start_date, n_months)
+    n_days = (end_date-start_date).days
+    n_hours = int(n_days*24/n_hours-n_hours)
+
+    release_times = []
+    for i in range(n_hours):
+        release_times.append(start_date+timedelta(hours=i))
+
+    return np.array(release_times)
+
+def get_n_daily_release_times(year:int, month:int, day:int, n_months=4, n_days=1) -> np.ndarray:
+    start_date = datetime(year, month, day)
+    end_date = add_month_to_time(start_date, n_months)
+    n_days = (end_date-start_date).days
+
+    release_times = []
+    for i in range(n_days):
+        release_times.append(start_date+timedelta(days=i))
+
+    return np.array(release_times)
