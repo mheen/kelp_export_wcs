9d2834557dd99eed984e8cb2cbae9ee427fce167
mirjam <mirjam.vandermheen@uwa.edu.au>
2023-04-03 09:04:29 +0800

    Added location

--- location_info.py
+++ location_info.py
@@ -12,6 +12,9 @@ class LocationInfo:
 perth = LocationInfo('perth', [115.25, 115.87], [-32.65, -31.49],
                      [115.3, 115.7], [-32.4, -32.0, -31.6],
                      [10, 25, 50, 100, 150, 200])
+perth_wide = LocationInfo('perth_wide', [114.0, 115.87], [-32.65, -31.49],
+                     [115.3, 115.7], [-32.4, -32.0, -31.6],
+                     [50, 100, 200])
 cwa_perth = LocationInfo('cwa_perth', [113.0, 115.87], [-34.33, -30.85],
                          [113.0, 114.0, 115,0], [-34.0, -33.0, -32.0, -31.0],
                          [10, 25, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000, 3000, 4000, 5000, 6000])
@@ -27,7 +30,7 @@ gsr = LocationInfo('gsr', [112.0, 155.0], [-45.0, -27.5],
 wa = LocationInfo('wa', [112.0, 116.0], [-34.0, -28.0],
                   [112.0, 114.0, 116.0], [-34.0, -32.0, -30.0, -28.0], [10, 50, 100, 200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000, 6000])
 
-all_locations = [perth, cwa_perth, cwa_perth_zoom, cwa_perth_zoom2, gsr, wa]
+all_locations = [perth, cwa_perth, cwa_perth_zoom, cwa_perth_zoom2, gsr, wa, perth_wide]
 
 def get_location_info(location:str) -> LocationInfo:
     for loc in all_locations:
